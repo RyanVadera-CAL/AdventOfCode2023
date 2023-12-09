e8e20fe21410b6bfe7d63ef01a8560b93a59b7ae
Ryan Vadera <114505089+RyanVadera-CAL@users.noreply.github.com>
2023-12-09 03:07:20 +0000

    Absolutely disgusting solution

--- AdventOfCode2023/AdventOfCode2023/Day05.cs
+++ AdventOfCode2023/AdventOfCode2023/Day05.cs
@@ -2,12 +2,62 @@ namespace AdventOfCode2023;
 
 public class Day05
 {
-    private readonly HashSet<long> _seeds;
+    private HashSet<long> _seeds = new();
     private readonly Queue<string> _lines;
 
     public Day05(string[] lines)
     {
         _lines = new Queue<string>(lines);
+    }
+    
+    public long Solve_A()
+    {
+        LoadSeeds_A();
+        
+        var locations = FollowMaps();
+
+        return locations.Min();
+    }
+
+    public long Solve_B()
+    {
+        var pairs = _lines
+            .Dequeue()
+            .Split(':')
+            [1].Split(' ')
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Select(long.Parse)
+            .ToList();
+
+        var seedPairs = new List<(long Start, long End)>();
+
+        for (var i = 0; i < pairs.Count / 2; i++)
+        {
+            var s = pairs.Skip(i * 2).Take(2);
+            seedPairs.Add(new ValueTuple<long, long>(s.First(), s.First() + s.Last()));
+        }
+
+        var maps = LoadMaps().Reverse();
+
+        long location = 0;
+
+        while (true)
+        {
+            var seed = maps.Aggregate(location, (current, map) => map.ReverseMapNumber(current));
+
+            if (location >= 24261546) throw new ApplicationException();
+
+            if (seedPairs.Any(sp => seed >= sp.Item1 && seed <= sp.Item2))
+            {
+                return location;
+            }
+
+            location++;
+        }
+    }
+
+    private void LoadSeeds_A()
+    {
         _seeds = _lines
             .Dequeue()
             .Split(':')
@@ -17,7 +67,7 @@ public class Day05
             .ToHashSet();
     }
     
-    public long Solve_A()
+    private HashSet<long> FollowMaps()
     {
         var maps = LoadMaps();
 
@@ -30,7 +80,7 @@ public class Day05
             locations.Add(x);
         }
 
-        return locations.Min();
+        return locations;
     }
 
     private ICollection<Map> LoadMaps()
@@ -75,31 +125,59 @@ public class Day05
 
             return source;
         }
+        
+        public long ReverseMapNumber(long dest)
+        {
+            foreach (var mapRange in MapRanges)
+            {
+                if (mapRange.TryReverseMapNumber(dest, out var source))
+                {
+                    return source!.Value;
+                }
+            }
+            
+            return dest;
+        }
     }
 
     private class MapRange
     {
-        private readonly long _start;
-        private readonly long _end;
+        private readonly long _sourceStart;
+        private readonly long _sourceEnd;
+        private readonly long _destStart;
+        private readonly long _destEnd;
         private readonly long _modifier;
         
         public MapRange(long destStart, long sourceStart, long length)
         {
-            _start = sourceStart;
-            _end = sourceStart + length;
+            _sourceStart = sourceStart;
+            _sourceEnd = sourceStart + length - 1;
+            _destStart = destStart;
+            _destEnd = destStart + length - 1;
             _modifier = destStart - sourceStart;
         }
         
-        public bool TryMapNumber(long source, out long? x)
+        public bool TryMapNumber(long source, out long? dest)
         {
-            x = null;
-            if (source < _start || source > _end)
+            if (source < _sourceStart || source > _sourceEnd)
             {
-                x = null;
+                dest = null;
                 return false;
             }
             
-            x = source + _modifier;
+            dest = source + _modifier;
+            return true;
+        }
+
+        public bool TryReverseMapNumber(long dest, out long? source)
+        {
+            if (dest < _destStart || dest > _destEnd)
+            {
+                source = null;
+                return false;
+            }
+            
+            source = dest - _modifier;
             return true;
         }
     }
